fc2eae67ab1fdb886d1bb0e438eaf0e9c2fe7686
tomms25 <tommasobaccano@gmail.com>
2023-01-31 18:07:12 +0100

    Bonus 1 stampa di tutti i dettagli oltre al titolo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
          @foreach ($movies as $movie)
 
             <li>
-               {{ $movie['title']}}
+               {{ $movie['title']}} - {{ $movie['original_title']}} - {{ $movie['nationality']}} - {{ $movie['date']}} - {{ $movie ['vote']}}
             </li>
              
          @endforeach
